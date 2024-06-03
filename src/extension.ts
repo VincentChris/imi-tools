1fdf8ab1fa98d739762eca4aabb1811748988624
vincent.xiao <vincentchris@126.com>
2024-06-03 19:01:48 +0800

    feat: 添加模板引擎

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
-
+import * as mustache from 'mustache';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
